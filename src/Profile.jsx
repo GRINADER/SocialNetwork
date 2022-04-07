cf634082e27f750c9ca1d1000de11cfefa39311c
GRINADER <46018576+GRINADER@users.noreply.github.com>
2022-04-07 17:26:39 +0300

    Add files via upload

---
+++ src/Profile.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Profile = () => {
+  return (
+    <div className="content">
+      <div>
+        <img src="https://img2.akspic.ru/previews/4/9/4/6/6/166494/166494-igra_v_kalmara_squid_game-500x.jpg" />
+        Main content
+      </div>
+      <div>ava + descr</div>
+      <div>
+        my posts
+        <div>new post</div>
+        <div>
+          <div>post 1</div>
+          <div>post 2</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
